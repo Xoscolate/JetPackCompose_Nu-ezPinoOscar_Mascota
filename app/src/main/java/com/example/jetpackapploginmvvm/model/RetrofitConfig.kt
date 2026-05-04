67a5329e6c9a34fd32dc209eeb1a798b3bde558f
Oscar Nunez <oscarnunezpino@gmail.com>
2026-05-04 21:22:23 +0200

    final final

--- app/src/main/java/com/example/jetpackapploginmvvm/model/RetrofitConfig.kt
+++ app/src/main/java/com/example/jetpackapploginmvvm/model/RetrofitConfig.kt
@@ -4,19 +4,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-data class MensajeApi(
-    val fact: String
-)
+data class MensajeApi(val fact: String)
 
-// 2. La ruta
 interface ApiService {
-    @GET("fact")
+    @GET("Xoscolate/JetPackCompose_Nu-ezPinoOscar_Mascota/refs/heads/consell/consell.json")
     suspend fun obtenerOraculo(): MensajeApi
 }
 
-// 3. El motor de Retrofit
 object RetrofitClient {
-    private const val BASE_URL = "https://raw.githubusercontent.com/Xoscolate/JetPackCompose_Nu-ezPinoOscar_Mascota/refs/heads/consell/consell.json" // URL base
+    private const val BASE_URL = "https://raw.githubusercontent.com/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
